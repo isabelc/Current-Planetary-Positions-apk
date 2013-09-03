ba5bcc8c3d741378d34e0b32f1ff2ef5f73ff286
Isabel Castillo <me@isabelcastillo.com>
2013-09-03 18:55:08 -0400

    ignore bin/ and gen/ and android project files

--- CurrentPlanetaryPositions/gen/org/astrolibrary/currentplanetarypositions/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.astrolibrary.currentplanetarypositions;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
