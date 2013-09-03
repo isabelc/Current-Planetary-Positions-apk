09e16bb6c1fd346870414a2ee513b421ae7e6098
Isabel Castillo <me@isabelcastillo.com>
2013-09-03 18:05:54 -0400

    "Initial commit for CPP for android"

---
+++ CurrentPlanetaryPositions/gen/org/astrolibrary/currentplanetarypositions/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.astrolibrary.currentplanetarypositions;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
